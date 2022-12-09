935becc4423068f0a2b225ff3c11252eca7bb2aa
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-08 23:15:44 -0700

    Updates

---
+++ precache-manifest.39d97aad70d6cd0536c75be4aa59c370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d18169f2098d211abe4ebdaad4ccea",
+    "url": "/card-list-react/index.html"
+  },
+  {
+    "revision": "12de803d39e06b2cd418",
+    "url": "/card-list-react/static/css/main.4daa7d03.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/card-list-react/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-list-react/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12de803d39e06b2cd418",
+    "url": "/card-list-react/static/js/main.0b057ccc.chunk.js"
+  },
+  {
+    "revision": "520945b4f32577abd18b",
+    "url": "/card-list-react/static/js/runtime-main.8ebb8169.js"
+  }
+]);
